3ad4456487e4658b92a6263ce50eb764252ebeda
“louis-27” <louis27isnow@gmail.com>
2022-01-24 16:39:21 +0700

    Initial commit

---
+++ solve.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define FOR(i, a) for (int i = 0; i < a; i++)
+#define DEBUG_IN freopen("input.txt", "r", stdin)
+#define DEBUG_OUT freopen("output.txt", "w", stdout)
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+void solve() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while (T--)
+        solve();
+
+    return 0;
+}
